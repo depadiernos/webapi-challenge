705b37d5762a5ae851d0bad24d9197d0eee14a1d
Dennis Padiernos <depadiernos@gmail.com>
2019-12-13 19:14:01 -0600

    implement App component, context and routes

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, createContext } from "react";
+import { Route, NavLink } from "react-router-dom";
+import Projects from "./components/Projects";
+import Project from "./components/Project";
+import "./App.css";
+
+export const navContext = createContext();
+
+export default function App() {
+  const [projects, setProjects] = useState();
+  const [currentAction, setCurrentAction] = useState();
+  const [currentProject, setCurrentProject] = useState();
+  return (
+    <navContext.Provider value={{currentProject, currentAction, projects, setCurrentProject, setCurrentAction, setProjects}}>
+      <div className="App">
+        <nav>
+          <NavLink to='/'></NavLink>
+        </nav>
+        <Route exact path="/" component={Projects} />
+        <Route path="/:id" component={Project} />
+      </div>
+    </navContext.Provider>
+  );
+}
