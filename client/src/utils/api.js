920d6a57ba5045811941159af0f95e5436883e31
Dennis Padiernos <depadiernos@gmail.com>
2019-12-13 20:54:28 -0600

    correctly send headers and body

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,24 +1,28 @@
 import fetch from "node-fetch";
 
 const url = "http://localhost:5000";
+const headers = {
+  Accept: "application/json",
+  "Content-Type": "application/json"
+};
 
 export const getProjects = async () => {
-    try{
-  const res = await fetch(`${url}/api/projects`, { method: "GET" });
-  if (res.status >= 200 && res.status < 300) {
-    const data = await res.json();
-    return data
+  try {
+    const res = await fetch(`${url}/api/projects`, { method: "GET" });
+    if (res.status >= 200 && res.status < 300) {
+      const data = await res.json();
+      return data;
+    }
+  } catch (err) {
+    console.log(err);
   }
-} catch(err){
-    console.log(err)
-}
 };
 
 export const getProject = async id => {
   const res = await fetch(`${url}/api/projects/${id}`, { method: "GET" });
   if (res.status >= 200 && res.status < 300) {
     const data = await res.json();
-    return data
+    return data;
   }
 };
 
@@ -28,7 +32,7 @@ export const getActions = async id => {
   });
   if (res.status >= 200 && res.status < 300) {
     const data = await res.json();
-    return data
+    return data;
   }
 };
 export const getAction = async (id, actionId) => {
@@ -37,47 +41,54 @@ export const getAction = async (id, actionId) => {
   });
   if (res.status >= 200 && res.status < 300) {
     const data = await res.json();
-    return data
+    return data;
   }
 };
 
 export const addProject = async data => {
   const res = await fetch(`${url}/api/projects`, {
     method: "POST",
-    body: data
+    headers: headers,
+    body: JSON.stringify(data)
   });
   if (res.status >= 200 && res.status < 300) {
     const data = await res.json();
-    return data
+    return data;
   }
 };
 
 export const addAction = async (id, data) => {
   const res = await fetch(`${url}/api/projects/${id}/actions`, {
     method: "POST",
-    body: data
+    headers: headers,
+    body: JSON.stringify(data)
   });
   if (res.status >= 200 && res.status < 300) {
     const data = await res.json();
-    return data
+    return data;
   }
 };
 
 export const editProject = async (id, data) => {
-  const res = await fetch(`${url}/api/projects/${id}`, { method: "PUT", body: data });
+  const res = await fetch(`${url}/api/projects/${id}`, {
+    method: "PUT",
+    headers: headers,
+    body: JSON.stringify(data)
+  });
   if (res.status >= 200 && res.status < 300) {
     const data = await res.json();
-    return data
+    return data;
   }
 };
 
 export const editAction = async (id, actionId, data) => {
   const res = await fetch(`${url}/api/projects/${id}/actions/${actionId}`, {
     method: "PUT",
-    body: data
+    headers: headers,
+    body: JSON.stringify(data)
   });
   if (res.status >= 200 && res.status < 300) {
     const data = await res.json();
-    return data
+    return data;
   }
 };
