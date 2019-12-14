2abc45b6ebeafe208d4cd1c02f0fb49fcc3d2058
Dennis Padiernos <depadiernos@gmail.com>
2019-12-13 19:10:54 -0600

    implement projectsRouter

---
+++ projects/projects.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const db = require("../data/helpers/projectModel");
+const { validateProject, validateProjectId } = require("../middlewares");
+
+const router = express.Router();
+
+router.post("/", validateProject(), async (req, res, next) => {
+  try {
+    const project = { name: req.body.name, description: req.body.description };
+    const newProject = await db.insert(project);
+    res.status(201).json(newProject);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/", async (req, res, next) => {
+  try {
+    const projects = await db.get();
+    res.status(200).json(projects);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/:id", validateProjectId(), (req, res, next) => {
+  try {
+    res.status(200).json(req.project);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", validateProjectId(), async (req, res, next) => {
+  try {
+    const deletedProject = await db.remove(req.params.id);
+    if (deletedProject) res.status(204).json(req.project);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put(
+  "/:id",
+  validateProject(),
+  validateProjectId(),
+  async (req, res, next) => {
+    try {
+      const project = {
+        name: req.body.name,
+        description: req.body.description
+      };
+      const success = await db.update(req.params.id, project);
+      const newProject = success && (await db.getById(req.params.id));
+      res.status(200).json(newProject);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get("/:id/actions", validateProjectId(), async (req, res, next) => {
+  try {
+    const actions = await db.getProjectActions(req.params.id);
+    res.status(200).json(actions);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
